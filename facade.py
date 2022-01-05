8a49d9ccff2d8da8fa234f9008e90006c5844d82
Gwennin <ggwennin@gmail.com>
2022-01-05 11:28:07 +0100

    lien corrigé

--- facade.py
+++ facade.py
@@ -6,10 +6,10 @@ class Facade():
     def __init__(self):
         self.mooringList = list()
         self.libraryList= list()
-        self.libraryList.append(libraryElement("Benthos","viewTest\\ressources\\pictures\\1Benthos_1m_chaine.bmp") )
-        self.libraryList.append(libraryElement("cable","viewTest\\ressources\\pictures\\cable.bmp") )
-        self.libraryList.append(libraryElement("chaine","viewTest\\ressources\\pictures\\chaine.bmp") )
-        self.libraryList.append(libraryElement("lest","viewTest\\ressources\\pictures\\lest.bmp") )
+        self.libraryList.append(libraryElement("Benthos","ressources\\pictures\\1Benthos_1m_chaine.bmp") )
+        self.libraryList.append(libraryElement("cable","ressources\\pictures\\cable.bmp") )
+        self.libraryList.append(libraryElement("chaine","ressources\\pictures\\chaine.bmp") )
+        self.libraryList.append(libraryElement("lest","ressources\\pictures\\lest.bmp") )
        
     def getLibrary(self):
         return self.libraryList
